39434143b88f023ee9f56a9319be56ada544412d
abilioferreira <75483023+abilioferreira@users.noreply.github.com>
2022-04-08 13:34:52 -0300

    topico 4

---
+++ lib/Repository/Repository.dart
@@ -0,0 +1,9 @@
+import "package:project_flutter/lib/Model/wordpair.dart";
+
+class wordPairRepositoty{
+  final List<wordPair> pairs = <wordPair>[];
+  final List<wordPair> saved = <wordPair>[];
+
+  wordPairRepositoty();
+}
+
